af2d0ffd162f9f74e19caa80cffde383b58b7273
lenhathuy <huyhuy2899@gmail.com>
2024-01-07 21:45:59 +0700

    [ADD] add chatbot logic

---
+++ chatbot.py
@@ -0,0 +1,375 @@
+import pandas as pd
+import numpy as np
+import re,string
+from gensim.models import KeyedVectors
+from collections import Counter
+from underthesea import word_tokenize
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf
+from tensorflow.keras import preprocessing, utils, activations
+# from keras.callbacks import ModelCheckpoint
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# Hyper-parameters
+NUM_LAYERS = 2
+NUM_HEADS = 6 # model dims must divided to number of heads
+UNITS = 512
+DROPOUT = 0.1
+VOCAB_SIZE  = 255
+embeddings_dim = 300
+maxlen_questions = 12
+maxlen_answers =67
+
+embedding_matrix = np.zeros((VOCAB_SIZE, embeddings_dim))
+
+def scaled_dot_product_attention(query, key, value, mask):
+  """Calculate the attention weights. """
+  matmul_qk = tf.matmul(query, key, transpose_b=True)
+
+  # scale matmul_qk
+  depth = tf.cast(tf.shape(key)[-1], tf.float32)
+  logits = matmul_qk / tf.math.sqrt(depth)
+
+  # add the mask to zero out padding tokens
+  if mask is not None:
+    logits += (mask * -1e9)
+
+  # softmax is normalized on the last axis (seq_len_k)
+  attention_weights = tf.nn.softmax(logits, axis=-1)
+
+  output = tf.matmul(attention_weights, value)
+
+  return output
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+
+  def __init__(self, d_model, num_heads, name="multi_head_attention"):
+    super(MultiHeadAttention, self).__init__(name=name)
+    self.num_heads = num_heads
+    self.d_model = d_model
+
+    assert d_model % self.num_heads == 0
+
+    self.depth = d_model // self.num_heads
+
+    self.query_dense = tf.keras.layers.Dense(units=d_model)
+    self.key_dense = tf.keras.layers.Dense(units=d_model)
+    self.value_dense = tf.keras.layers.Dense(units=d_model)
+
+    self.dense = tf.keras.layers.Dense(units=d_model)
+
+  def split_heads(self, inputs, batch_size):
+    inputs = tf.reshape(
+        inputs, shape=(batch_size, -1, self.num_heads, self.depth))
+    return tf.transpose(inputs, perm=[0, 2, 1, 3])
+
+  def call(self, inputs):
+    query, key, value, mask = inputs['query'], inputs['key'], inputs[
+        'value'], inputs['mask']
+    batch_size = tf.shape(query)[0]
+
+    # linear layers
+    query = self.query_dense(query)
+    key = self.key_dense(key)
+    value = self.value_dense(value)
+
+    # split heads
+    query = self.split_heads(query, batch_size)
+    key = self.split_heads(key, batch_size)
+    value = self.split_heads(value, batch_size)
+
+    # scaled dot-product attention
+    scaled_attention = scaled_dot_product_attention(query, key, value, mask)
+
+    scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+
+    # concatenation of heads
+    concat_attention = tf.reshape(scaled_attention,
+                                  (batch_size, -1, self.d_model))
+
+    # final linear layer
+    outputs = self.dense(concat_attention)
+
+    return outputs
+
+def create_padding_mask(x):
+  mask = tf.cast(tf.math.equal(x, 0), tf.float32)
+  # (batch_size, 1, 1, sequence length)
+  return mask[:, tf.newaxis, tf.newaxis, :]
+
+def create_look_ahead_mask(x):
+  seq_len = tf.shape(x)[1]
+  look_ahead_mask = 1 - tf.linalg.band_part(tf.ones((seq_len, seq_len)), -1, 0)
+  padding_mask = create_padding_mask(x)
+  return tf.maximum(look_ahead_mask, padding_mask)
+
+class PositionalEncoding(tf.keras.layers.Layer):
+
+  def __init__(self, position, d_model):
+    super(PositionalEncoding, self).__init__()
+    self.pos_encoding = self.positional_encoding(position, d_model)
+
+  def get_angles(self, position, i, d_model):
+    angles = 1 / tf.pow(10000, (2 * (i // 2)) / tf.cast(d_model, tf.float32))
+    return position * angles
+
+  def positional_encoding(self, position, d_model):
+    angle_rads = self.get_angles(
+        position=tf.range(position, dtype=tf.float32)[:, tf.newaxis],
+        i=tf.range(d_model, dtype=tf.float32)[tf.newaxis, :],
+        d_model=d_model)
+    # apply sin to even index in the array
+    sines = tf.math.sin(angle_rads[:, 0::2])
+    # apply cos to odd index in the array
+    cosines = tf.math.cos(angle_rads[:, 1::2])
+
+    pos_encoding = tf.concat([sines, cosines], axis=-1)
+    pos_encoding = pos_encoding[tf.newaxis, ...]
+    return tf.cast(pos_encoding, tf.float32)
+
+  def call(self, inputs):
+    return inputs + self.pos_encoding[:, :tf.shape(inputs)[1], :]
+
+def encoder_layer(units, d_model, num_heads, dropout, name="encoder_layer"):
+  inputs = tf.keras.Input(shape=(None, d_model), name="inputs")
+  padding_mask = tf.keras.Input(shape=(1, 1, None), name="padding_mask")
+
+  attention = MultiHeadAttention(
+      d_model, num_heads, name="attention")({
+          'query': inputs,
+          'key': inputs,
+          'value': inputs,
+          'mask': padding_mask
+      })
+  attention = tf.keras.layers.Dropout(rate=dropout)(attention)
+  attention = tf.keras.layers.LayerNormalization(
+      epsilon=1e-6)(inputs + attention)
+
+  outputs = tf.keras.layers.Dense(units=units, activation='relu')(attention)
+  outputs = tf.keras.layers.Dense(units=d_model)(outputs)
+  outputs = tf.keras.layers.Dropout(rate=dropout)(outputs)
+  outputs = tf.keras.layers.LayerNormalization(
+      epsilon=1e-6)(attention + outputs)
+
+  return tf.keras.Model(
+      inputs=[inputs, padding_mask], outputs=outputs, name=name)
+
+def encoder(vocab_size,
+            num_layers,
+            units,
+            d_model,
+            num_heads,
+            dropout,
+            name="encoder"):
+  inputs = tf.keras.Input(shape=(None,), name="inputs")
+  padding_mask = tf.keras.Input(shape=(1, 1, None), name="padding_mask")
+
+  embeddings = tf.keras.layers.Embedding(vocab_size,d_model
+                                     ,input_length=maxlen_questions
+                                     ,weights = [embedding_matrix]
+                                     ,trainable=False)(inputs)
+  embeddings *= tf.math.sqrt(tf.cast(d_model, tf.float32))
+  embeddings = PositionalEncoding(vocab_size, d_model)(embeddings)
+
+  outputs = tf.keras.layers.Dropout(rate=dropout)(embeddings)
+
+  for i in range(num_layers):
+    outputs = encoder_layer(
+        units=units,
+        d_model=d_model,
+        num_heads=num_heads,
+        dropout=dropout,
+        name="encoder_layer_{}".format(i),
+    )([outputs, padding_mask])
+
+  return tf.keras.Model(
+      inputs=[inputs, padding_mask], outputs=outputs, name=name)
+  
+def decoder_layer(units, d_model, num_heads, dropout, name="decoder_layer"):
+  inputs = tf.keras.Input(shape=(None, d_model), name="inputs")
+  enc_outputs = tf.keras.Input(shape=(None, d_model), name="encoder_outputs")
+  look_ahead_mask = tf.keras.Input(
+      shape=(1, None, None), name="look_ahead_mask")
+  padding_mask = tf.keras.Input(shape=(1, 1, None), name='padding_mask')
+
+  attention1 = MultiHeadAttention(
+      d_model, num_heads, name="attention_1")(inputs={
+          'query': inputs,
+          'key': inputs,
+          'value': inputs,
+          'mask': look_ahead_mask
+      })
+  attention1 = tf.keras.layers.LayerNormalization(
+      epsilon=1e-6)(attention1 + inputs)
+
+  attention2 = MultiHeadAttention(
+      d_model, num_heads, name="attention_2")(inputs={
+          'query': attention1,
+          'key': enc_outputs,
+          'value': enc_outputs,
+          'mask': padding_mask
+      })
+  attention2 = tf.keras.layers.Dropout(rate=dropout)(attention2)
+  attention2 = tf.keras.layers.LayerNormalization(
+      epsilon=1e-6)(attention2 + attention1)
+
+  outputs = tf.keras.layers.Dense(units=units, activation='relu')(attention2)
+  outputs = tf.keras.layers.Dense(units=d_model)(outputs)
+  outputs = tf.keras.layers.Dropout(rate=dropout)(outputs)
+  outputs = tf.keras.layers.LayerNormalization(
+      epsilon=1e-6)(outputs + attention2)
+
+  return tf.keras.Model(
+      inputs=[inputs, enc_outputs, look_ahead_mask, padding_mask],
+      outputs=outputs,
+      name=name)
+  
+def decoder(vocab_size,
+            num_layers,
+            units,
+            d_model,
+            num_heads,
+            dropout,
+            name='decoder'):
+  inputs = tf.keras.Input(shape=(None,), name='inputs')
+  enc_outputs = tf.keras.Input(shape=(None, d_model), name='encoder_outputs')
+  look_ahead_mask = tf.keras.Input(
+      shape=(1, None, None), name='look_ahead_mask')
+  padding_mask = tf.keras.Input(shape=(1, 1, None), name='padding_mask')
+
+
+  embeddings = tf.keras.layers.Embedding(vocab_size,d_model
+                                     ,input_length=maxlen_answers
+                                     ,weights = [embedding_matrix]
+                                     ,trainable=False)(inputs)
+  embeddings *= tf.math.sqrt(tf.cast(d_model, tf.float32))
+  embeddings = PositionalEncoding(vocab_size, d_model)(embeddings)
+
+  outputs = tf.keras.layers.Dropout(rate=dropout)(embeddings)
+
+  for i in range(num_layers):
+    outputs = decoder_layer(
+        units=units,
+        d_model=d_model,
+        num_heads=num_heads,
+        dropout=dropout,
+        name='decoder_layer_{}'.format(i),
+    )(inputs=[outputs, enc_outputs, look_ahead_mask, padding_mask])
+
+  return tf.keras.Model(
+      inputs=[inputs, enc_outputs, look_ahead_mask, padding_mask],
+      outputs=outputs,
+      name=name)
+  
+def transformer(vocab_size,
+                num_layers,
+                units,
+                d_model,
+                num_heads,
+                dropout,
+                name="transformer"):
+  inputs = tf.keras.Input(shape=(None,), name="inputs")
+  dec_inputs = tf.keras.Input(shape=(None,), name="dec_inputs")
+
+  enc_padding_mask = tf.keras.layers.Lambda(
+      create_padding_mask, output_shape=(1, 1, None),
+      name='enc_padding_mask')(inputs)
+  # mask the future tokens for decoder inputs at the 1st attention block
+  look_ahead_mask = tf.keras.layers.Lambda(
+      create_look_ahead_mask,
+      output_shape=(1, None, None),
+      name='look_ahead_mask')(dec_inputs)
+  # mask the encoder outputs for the 2nd attention block
+  dec_padding_mask = tf.keras.layers.Lambda(
+      create_padding_mask, output_shape=(1, 1, None),
+      name='dec_padding_mask')(inputs)
+
+  enc_outputs = encoder(
+      vocab_size=vocab_size,
+      num_layers=num_layers,
+      units=units,
+      d_model=d_model,
+      num_heads=num_heads,
+      dropout=dropout,
+  )(inputs=[inputs, enc_padding_mask])
+
+  dec_outputs = decoder(
+      vocab_size=vocab_size,
+      num_layers=num_layers,
+      units=units,
+      d_model=d_model,
+      num_heads=num_heads,
+      dropout=dropout,
+  )(inputs=[dec_inputs, enc_outputs, look_ahead_mask, dec_padding_mask])
+
+  outputs = tf.keras.layers.Dense(units=vocab_size)(dec_outputs)
+  #Add a softmax layer to get probability distribution of word in vocab
+  outputs = tf.keras.layers.Dense(units=vocab_size, activation = 'softmax',name = "outputs")(outputs)
+
+
+  return tf.keras.Model(inputs=[inputs, dec_inputs], outputs=outputs, name=name)
+
+tf.keras.backend.clear_session()
+
+# Define the model
+model = transformer(
+    vocab_size=VOCAB_SIZE,
+    num_layers=NUM_LAYERS,
+    units=UNITS,
+    d_model=embeddings_dim,
+    num_heads=NUM_HEADS,
+    dropout=DROPOUT)
+
+model.load_weights('chatbot_weights.h5')
+
+tokenizer = Tokenizer()
+
+word2idx = tokenizer.word_index
+
+idx2word = tokenizer.index_word
+
+def str_to_tokens(sentence):
+    words = word_tokenize(sentence.lower())
+    tokens_list = []
+
+    for word in words:
+        tokens_list.append(word2idx[word])
+    return pad_sequences([tokens_list],maxlen = maxlen_questions , padding='post')
+
+def evaluate(sentence):
+  sentence = str_to_tokens(sentence)
+  output = np.zeros((1, 1))
+  output[0, 0] = word2idx['start']
+
+  for i in range(maxlen_answers):
+    predictions = model(inputs=[sentence, output], training=False)
+
+    # select the last word from the seq_len dimension
+    predictions = predictions[:, -1:, :]
+    predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+
+    # return the result if the predicted_id is equal to the end token
+    if tf.equal(predicted_id, word2idx['end']):
+      break
+
+    # concatenated the predicted_id to the output which is given to the decoder as its input.
+    output = tf.concat([output, predicted_id], axis=-1)
+
+  return tf.squeeze(output, axis=0)
+
+def predict(sentence):
+  prediction = evaluate(sentence)
+  predicted_sentence = " ".join(idx2word[tf.get_static_value(i)] for i in prediction if i < VOCAB_SIZE)
+  return predicted_sentence.replace("start","")
+
+def getPredict(sentence):
+  if sentence != 'bye':
+      try:
+          return 'BOT: ' + predict(sentence)
+      except:
+          return "BOT: Xin lỗi câu này tôi chưa đc học ,vui lòng hỏi lại"
+  else:
+      return "BOT: Tạm biệt nha!"
--- server_chat.py
+++ server_chat.py
@@ -1,79 +1,19 @@
-# import nltk
-# nltk.download('popular')
-# from nltk.stem import WordNetLemmatizer
-# lemmatizer = WordNetLemmatizer()
-# import pickle
-# import numpy as np
-
-# from keras.models import load_model
-# model = load_model('model.h5')
-# import json
-# import random
-# intents = json.loads(open('data.json').read())
-# words = pickle.load(open('texts.pkl','rb'))
-# classes = pickle.load(open('labels.pkl','rb'))
-
-# def clean_up_sentence(sentence):
-#     # tokenize the pattern - split words into array
-#     sentence_words = nltk.word_tokenize(sentence)
-#     # stem each word - create short form for word
-#     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
-#     return sentence_words
-
-# # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-
-# def bow(sentence, words, show_details=True):
-#     # tokenize the pattern
-#     sentence_words = clean_up_sentence(sentence)
-#     # bag of words - matrix of N words, vocabulary matrix
-#     bag = [0]*len(words)  
-#     for s in sentence_words:
-#         for i,w in enumerate(words):
-#             if w == s: 
-#                 # assign 1 if current word is in the vocabulary position
-#                 bag[i] = 1
-#                 if show_details:
-#                     print ("found in bag: %s" % w)
-#     return(np.array(bag))
-
-# def predict_class(sentence, model):
-#     # filter out predictions below a threshold
-#     p = bow(sentence, words,show_details=False)
-#     res = model.predict(np.array([p]))[0]
-#     ERROR_THRESHOLD = 0.25
-#     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-#     # sort by strength of probability
-#     results.sort(key=lambda x: x[1], reverse=True)
-#     return_list = []
-#     for r in results:
-#         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
-#     return return_list
-
-# def getResponse(ints, intents_json):
-#     tag = ints[0]['intent']
-#     list_of_intents = intents_json['intents']
-#     for i in list_of_intents:
-#         if(i['tag']== tag):
-#             result = random.choice(i['responses'])
-#             break
-#     return result
-
-# def chatbot_response(msg):
-#     ints = predict_class(msg, model)
-#     res = getResponse(ints, intents)
-#     return res
+import chatbot
 
 
 from flask import Flask, render_template, request, jsonify
 import database.utils as db
+import whisper
 
 app = Flask(__name__)
 app.static_folder = 'static'
 
-# @app.route("/get")
-# def get_bot_response():
-#     userText = request.args.get('msg')
-#     return chatbot_response(userText)
+@app.route("/get_msg", methods = ["GET"])
+def get_bot_response():
+    userText = request.args.get('msg')
+    return jsonify({
+        "message": chatbot.getPredict(userText)
+    })
 
 @app.route("/school", methods = ["GET"])
 def getSchoolInfo():
@@ -252,6 +192,36 @@ def get_news_detail_info():
     }
 
     return jsonify(news_detail)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify( {
+        "message": 'No selected file'
+    })
+
+    file.save('/home/lenhathuy/android_project/TuVanTuyenSinhPTIT_server/file.mp3')
+
+    model = whisper.load_model("small")
+
+    audio = whisper.load_audio("/home/lenhathuy/android_project/TuVanTuyenSinhPTIT_server/file.mp3")
+
+    options = {
+        "language": "vi", 
+        "task": "transcribe" 
+    }
+    result = whisper.transcribe(model, audio, **options)
+    # result = model.transcribe("/home/lenhathuy/android_project/TuVanTuyenSinhPTIT_server/file.mp3")
+
+    print(result["text"])
+    return  jsonify( {
+        "message": result["text"]
+    })
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001)
